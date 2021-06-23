17f116d1ad9f6a3009d5121bbacc2d5198103695
CryptNation <lewisdennisevans@hotmail.com>
2021-06-23 20:34:41 +0100

    Updated to 1.16.5 Also added Void Crop.json

---
+++ src/main/java/com/cryptnation/cryptsadditions/crops/VoidCrop.java
@@ -0,0 +1,12 @@
+package com.cryptnation.cryptsadditions.crops;
+
+import net.minecraft.block.CropsBlock;
+
+public class VoidCrop extends CropsBlock
+{
+
+
+    public VoidCrop(Properties builder) {
+        super(builder);
+    }
+}
