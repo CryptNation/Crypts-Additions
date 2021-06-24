61668305220b726b7d57053c5c87ffc451c41c0a
CryptNation <lewisdennisevans@hotmail.com>
2021-06-24 11:33:39 +0100

    New Branch

--- src/main/java/com/cryptnation/cryptsadditions/item/food/deBar.java
+++ src/main/java/com/cryptnation/cryptsadditions/item/food/deBar.java
@@ -3,6 +3,7 @@ package com.cryptnation.cryptsadditions.item.food;
 import com.cryptnation.cryptsadditions.CryptsAdditions;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
@@ -14,5 +15,11 @@ public class deBar extends Item
         super(new Properties().group(CryptsAdditions.CRYPTS_ADDITIONS)
         .food(new Food.Builder().hunger(20).saturation(1.5f)
                 .effect(() -> new EffectInstance(Effects.GLOWING, 500, 2),0.8f).build()));
+    }
+
+    @Override
+    public int getBurnTime(ItemStack itemStack)
+    {
+        return 6000;
     }
 }
