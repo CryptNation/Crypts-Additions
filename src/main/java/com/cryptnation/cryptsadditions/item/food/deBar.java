bd7d190371af1062bb6ea684b32b737614e5d96c
CryptNation <lewisdennisevans@hotmail.com>
2021-07-17 13:32:10 +0100

    Tooltip - DE'Bar

--- src/main/java/com/cryptnation/cryptsadditions/item/food/deBar.java
+++ src/main/java/com/cryptnation/cryptsadditions/item/food/deBar.java
@@ -1,12 +1,18 @@
 package com.cryptnation.cryptsadditions.item.food;
 
 import com.cryptnation.cryptsadditions.CryptsAdditions;
+import com.cryptnation.cryptsadditions.utilities.KeyBoardHelper;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.World;
+
+import java.util.List;
 
 public class deBar extends Item
 {
@@ -21,5 +27,20 @@ public class deBar extends Item
     public int getBurnTime(ItemStack itemStack)
     {
         return 6000;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, World world, List<ITextComponent> tooltip,
+        ITooltipFlag flag)
+    {
+        if (KeyBoardHelper.isHoldingShift())
+        {
+            tooltip.add(new StringTextComponent(":HINT: Maybe.. This could tame a Guardian?"));
+        }
+        else
+        {
+            tooltip.add(new StringTextComponent("A mystical bar, originated from the Void Dimension"));
+        }
+        super.addInformation(stack, world, tooltip, flag);
     }
 }
